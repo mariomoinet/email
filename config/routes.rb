15edcc326e263bf960b1ae1b0988c9f22703c1b7
mariomoinet <mariomoinet@outlook.fr>
2017-08-31 14:49:19 +0200

    1

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'emails#index'
+  get 'emails/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
